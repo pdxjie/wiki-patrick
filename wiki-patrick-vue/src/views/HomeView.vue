b625792771640b76ea6f0c9055161064b59f2bfe
派 大 星 <none-j@qq.com>
2022-07-07 23:19:37 +0800

    拆分the-header 和 the-footer组件

---
+++ wiki-patrick-vue/src/views/HomeView.vue
@@ -0,0 +1,72 @@
+<template>
+  <a-layout>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+          mode="inline"
+          :default-selected-keys="['1']"
+          :default-open-keys="['sub1']"
+          :style="{ height: '100%', borderRight: 0 }"
+      >
+        <a-sub-menu key="sub1">
+          <span slot="title"><a-icon type="user" />subnav 1111</span>
+          <a-menu-item key="1">
+            option1
+          </a-menu-item>
+          <a-menu-item key="2">
+            option2
+          </a-menu-item>
+          <a-menu-item key="3">
+            option3
+          </a-menu-item>
+          <a-menu-item key="4">
+            option4
+          </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title"><a-icon type="laptop" />subnav 2</span>
+          <a-menu-item key="5">
+            option5
+          </a-menu-item>
+          <a-menu-item key="6">
+            option6
+          </a-menu-item>
+          <a-menu-item key="7">
+            option7
+          </a-menu-item>
+          <a-menu-item key="8">
+            option8
+          </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <span slot="title"><a-icon type="notification" />subnav 3</span>
+          <a-menu-item key="9">
+            option9
+          </a-menu-item>
+          <a-menu-item key="10">
+            option10
+          </a-menu-item>
+          <a-menu-item key="11">
+            option11
+          </a-menu-item>
+          <a-menu-item key="12">
+            option12
+          </a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      电子书列表
+    </a-layout-content>
+  </a-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default Vue.extend({
+  name: 'HomeView'
+});
+</script>
