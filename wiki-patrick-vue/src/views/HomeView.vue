b7a10bc01a04afbc110a3bee8054706bc2dad6ee
派 大 星 <none-j@qq.com>
2022-07-08 00:25:47 +0800

    将电子书渲染到界面并修改cover样式

--- wiki-patrick-vue/src/views/HomeView.vue
+++ wiki-patrick-vue/src/views/HomeView.vue
@@ -57,26 +57,85 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      电子书列表
+      <!--电子书列表-->
+      <a-list item-layout="vertical" size="large" :grid="{gutter:20,column:3}" :data-source="ebooks">
+        <a-list-item slot="renderItem" key="item.name" slot-scope="item">
+          <template v-for="{ type, text } in actions" slot="actions">
+        <span :key="type">
+          <a-icon :type="type" style="margin-right: 8px" />
+          {{ text }}
+        </span>
+          </template>
+          <a-list-item-meta :description="item.description">
+            <a slot="title" :href="item.href">{{ item.name }}</a>
+            <a-avatar slot="avatar" :src="item.cover" />
+          </a-list-item-meta>
+        </a-list-item>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue, {onMounted, ref} from 'vue';
 import axios from 'axios'
+
+
+const listData:any = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
 export default Vue.extend({
   name: 'HomeView',
   // Vue3新增的初始化方法
   setup(){
     console.log('setup')
+    const ebooks = ref()
     const queryVo = {
       id:null,
       name:null
     }
-    axios.post('http://localhost:3031/ebook/list',queryVo).then((res) =>{
-      console.log(res)
+    onMounted(()=>{
+      console.log("onMounted")
+      axios.post('http://localhost:3031/ebook/list',queryVo).then((res) =>{
+        const data = res.data.data
+        ebooks.value = data
+      });
     })
+    return{
+      ebooks,
+      listData,
+      pagination: {
+        onChange: (page:any) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions: [
+        { type: 'star-o', text: '156' },
+        { type: 'like-o', text: '156' },
+        { type: 'message', text: '2' },
+      ],
+    }
   }
 });
 </script>
+
+<style scoped>
+.ant-avatar{
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 8%;
+  margin: 5px 0;
+}
+</style>
