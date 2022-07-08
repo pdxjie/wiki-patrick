6416ff59e90bbd2f09eb4eac1fb406a446de5699
派 大 星 <none-j@qq.com>
2022-07-08 12:02:35 +0800

    配置axios拦截器

--- wiki-patrick-vue/src/main.ts
+++ wiki-patrick-vue/src/main.ts
@@ -8,8 +8,26 @@ import axios from "axios";
 // @ts-ignore
 axios.defaults.baseURL = process.env.VUE_APP_SERVER
 Vue.config.productionTip = false
-
 Vue.use(Antd)
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(config=>{
+  console.log('请求参数：',config)
+  return config
+},error => {
+  return Promise.reject(error)
+})
+axios.interceptors.response.use(response=>{
+  console.log('返回结果：',response)
+  return response
+},error => {
+  return Promise.reject(error)
+})
+
+
+
 
 new Vue({
   router,
